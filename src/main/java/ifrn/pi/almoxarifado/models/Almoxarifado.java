c3d99e4ca01a99714fe4fd6cb8392ccbdabe0094
Brenda Michelle <brendamichelle013@gmail.com>
2021-03-16 17:30:19 -0300

    Criando modelo para Almoxarifado

---
+++ src/main/java/ifrn/pi/almoxarifado/models/Almoxarifado.java
@@ -0,0 +1,20 @@
+package ifrn.pi.almoxarifado.models;
+
+public class Almoxarifado {
+
+	private String material;
+	private int quantidade;
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+}
